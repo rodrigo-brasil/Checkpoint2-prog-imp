66142759329a67cb8d43d59d07ba8d117ee711cb
Rodrigo Brasil <maic_rodrigo@hotmail.com>
2021-06-28 15:21:17 -0300

    adicionando testes

--- checkpoint.js
+++ checkpoint.js
@@ -5,20 +5,24 @@ function microondas() {
   this.carne = 15;
   this.feijao = 12;
   this.brigadeiro = 8;
-  //método para ligar o microondas podendo ou não passar um tempo fora do 
+  //método para ligar o microondas podendo ou não passar um tempo fora do tempo padrão
   this.ligar = (prato, tempo = this[prato]) => {
     if (this.hasOwnProperty(prato)) {
       if (tempo < this[prato]) console.log("tempo insuficiente");
       else if (tempo >= this[prato] * 3) console.log("kabumm");
       else if (tempo >= this[prato] * 2) console.log("comida queimou");
-      else console.log(`${prato} pronto em ${this[prato]}s, bom apetite!!!`);
+      else console.log(`${prato} pronto em ${tempo}s, bom apetite!!!`);
     } else {
       console.log("Prato inexistente");
     }
   };
 }
 
-let meuMicroondas = new microondas();
+let meuMicroondas = new microondas(); //Criando um novo Microondas
 
 //Testes
-meuMicroondas.ligar("pipoca", 1);
+meuMicroondas.ligar("pipoca", 1); // tempo insuficiente
+meuMicroondas.ligar("pipoca", 11); //pipoca pronto em 11s, bom apetite!!!
+meuMicroondas.ligar("pipoca", 20);  //comida queimou
+meuMicroondas.ligar("pipoca", 30); //kabumm
+meuMicroondas.ligar("Arroz", 12); //Prato inexistente
