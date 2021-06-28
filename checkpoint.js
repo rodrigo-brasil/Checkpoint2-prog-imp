dea2c0b0a82b2d7f5a624901ce0f45995278a682
Rodrigo Brasil <maic_rodrigo@hotmail.com>
2021-06-28 16:05:05 -0300

    testes

--- checkpoint.js
+++ checkpoint.js
@@ -1,3 +1,4 @@
+//função construtora
 function microondas() {
   // propriedades do obj menu
   this.pipoca = 10;
@@ -21,7 +22,8 @@ function microondas() {
 const meuMicroondas = new microondas(); //Criando um novo Microondas
 
 //Testes
-meuMicroondas.ligar("pipoca", 1); // tempo insuficiente
+meuMicroondas.ligar("brigadeiro"); //brigadeiro pronto em 8s, bom apetite!!!
+meuMicroondas.ligar("carne", 1); // tempo insuficiente
 meuMicroondas.ligar("pipoca", 11); //pipoca pronto em 11s, bom apetite!!!
 meuMicroondas.ligar("pipoca", 20);  //comida queimou
 meuMicroondas.ligar("pipoca", 30); //kabumm
